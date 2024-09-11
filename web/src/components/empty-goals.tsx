ab0843bd781e342398f7595ef505820f081fe87b
mmmenezes <mathmelomenezes@gmail.com>
2024-09-11 18:37:40 -0300

    commit inicial
    aula 1

---
+++ web/src/components/empty-goals.tsx
@@ -0,0 +1,23 @@
+import { Plus } from 'lucide-react'
+import logo from '../assets/logo-in-orbit.svg'
+import letsstart from '../assets/lets-start-illustration.svg'
+import { DialogTrigger } from './ui/dialog'
+import { Button } from './ui/button'
+
+export function EmptyGoals() {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center gap-8">
+      <img src={logo} alt="in.orbit" />
+      <img src={letsstart} alt="let's start" />
+      <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
+        Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora mesmo?
+      </p>
+      <DialogTrigger>
+        <Button>
+          <Plus className="size-4" />
+          Cadastrar Meta
+        </Button>
+      </DialogTrigger>
+    </div>
+  )
+}
